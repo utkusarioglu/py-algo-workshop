3a72c93c8b9deb68f13164aaff906ab77bead6ed
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-09 16:19:47 +0000

    Feat: Finalize maximum subarray
    - Finalize working version of maximum subarray with start and end
      indices.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #!/home/python/venv/main/bin/python
-from maximum_subarray import maximum_subarray_with_indices
+from maximum_subarray import maximum_subarray, maximum_subarray_with_indices
 
 if __name__ == "__main__":
   arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-  print(arr)
-  result = maximum_subarray_with_indices(arr)
-  print(result)
+  print(arr, len(arr))
+  print("vanilla", maximum_subarray(arr))
+  print("with_indices", maximum_subarray_with_indices(arr))
