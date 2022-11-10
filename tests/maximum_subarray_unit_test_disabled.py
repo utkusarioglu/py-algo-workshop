b78396e3fbde6fb0dc33abcd188cfd730a0f22d9
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-10 10:57:36 +0000

    Test: Refactor maximum_subarray tests
    - Refactor maximum_subarray tests to be inside the same file. This repo
      will get crowded with multiple tests, so it may be a good idea to
      simplify the repo layout.

---
+++ tests/maximum_subarray_unit_test_disabled.py
@@ -0,0 +1,33 @@
+#!/home/python/venv/main/bin/python
+import sys
+sys.path.append("..")
+import unittest
+from src import maximum_subarray
+
+class MaximumSubarrayTest(unittest.TestCase):
+  def test_default_values_vanilla(self):
+    param1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    expected = 6
+    result = maximum_subarray.maximum_subarray(param1)
+    self.assertEqual(expected, result)
+
+  def test_extended_values_vanilla(self):
+    param1 = [-2, 1, -3, 4, -1, 2, 1, -6, 4, 1, 1]
+    expected = 6
+    result = maximum_subarray.maximum_subarray(param1)
+    self.assertEqual(expected, result)
+
+  def test_default_values_indices(self):
+    param1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    expected = [6, 3, 6]
+    response = maximum_subarray.maximum_subarray_with_indices(param1)
+    self.assertEqual(expected, response)
+    
+  def test_extended_values_indices(self):
+    param1 = [-2, 1, -3, 4, -1, 2, 1, -6, 4, 1]
+    expected = [6, 3, 6]
+    response = maximum_subarray.maximum_subarray_with_indices(param1)
+    self.assertEqual(expected, response)
+
+if __name__ == "__main__":
+  unittest.main()
