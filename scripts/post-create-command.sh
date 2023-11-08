78fcccdf9fe6fbd41ca5f1d432e0c9c7574a428e
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-11-08 17:07:00 +0000

    Feat: Leetcode container with most water
    - Solved with a greedy algorithm

--- scripts/post-create-command.sh
+++ scripts/post-create-command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/opt/bitnami/python/bin/python -m pip install -U flake8
+# /home/python/venv/main/bin/python -m pip --require-virtualenv install -U flake8
 
 ~/elam/elam.sh repo status
